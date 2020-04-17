426d4052a0a88c65f76def1ce19ac6db322be979
Christopher Bartling <chris.bartling@gmail.com>
2020-04-17 10:43:32 -0500

    Started working on the StocksListingToolbar.

---
+++ src/views/stocks/list/StocksListingToolbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function StocksListingToolbar(props) {
+    return (
+        <div className="row">
+            <button className="btn btn-success">Add new stock</button>
+            <form className="form-inline my-2 my-lg-0 ">
+                <input className="form-control mr-sm-2 ml-sm-2"
+                       type="search"
+                       placeholder="Filter stocks"
+                       aria-label="Filter"/>
+                <button className="btn btn-outline-primary my-2 my-sm-0"
+                        type="submit">Filter</button>
+            </form>
+        </div>
+    );
+}
+
+export default StocksListingToolbar;
