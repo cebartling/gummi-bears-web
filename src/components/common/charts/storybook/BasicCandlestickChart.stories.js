6ba7ad60c8107762587c909174eae13553b2ff8e
Christopher Bartling <chris.bartling@gmail.com>
2020-05-25 15:54:02 -0500

    Basic candlestick chart

---
+++ src/components/common/charts/storybook/BasicCandlestickChart.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BasicCandlestickChart from '../BasicCandlestickChart';
+
+export default {
+    title: 'BasicCandlestickChart',
+    component: BasicCandlestickChart,
+};
+
+export const defaultComponent = () => {
+    return (
+        <BasicCandlestickChart/>
+    );
+}
