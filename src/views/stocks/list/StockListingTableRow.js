c22ac207ce306dfec6c3521c4095787044388629
Christopher Bartling <chris.bartling@gmail.com>
2020-09-10 19:40:16 -0500

    Adding classes for Cucumber.

--- src/views/stocks/list/StockListingTableRow.js
+++ src/views/stocks/list/StockListingTableRow.js
@@ -7,74 +7,74 @@ import StockPriceDirection from "../../../components/common/StockPriceDirection"
 
 const StocksListingTableRow = ({stock, userStockId}) => {
 
-    const {id, name, symbol, latestDailyTimeSeriesEvent} = stock;
-    const userStockDetailUrl = `/userStock/${userStockId}`;
+  const {id, name, symbol, latestDailyTimeSeriesEvent} = stock;
+  const userStockDetailUrl = `/userStock/${userStockId}`;
 
-    return (
-        <tr key={id}>
-            <td>
-                <Link to={userStockDetailUrl}>{name}</Link>
-            </td>
-            <td>{symbol}</td>
-            <td className="text-right">
-                <CurrencyFormat value={latestDailyTimeSeriesEvent?.openPrice}
-                                displayType={'text'}
-                                thousandSeparator={true}
-                                fixedDecimalScale={true}
-                                decimalScale={2}
-                                prefix={'$'}/>
-            </td>
-            <td className="text-right">
-                <CurrencyFormat value={latestDailyTimeSeriesEvent?.highPrice}
-                                displayType={'text'}
-                                thousandSeparator={true}
-                                fixedDecimalScale={true}
-                                decimalScale={2}
-                                prefix={'$'}/>
-            </td>
-            <td className="text-right">
-                <CurrencyFormat value={latestDailyTimeSeriesEvent?.lowPrice}
-                                displayType={'text'}
-                                thousandSeparator={true}
-                                fixedDecimalScale={true}
-                                decimalScale={2}
-                                prefix={'$'}/>
-            </td>
-            <td className="text-right">
-                <CurrencyFormat value={latestDailyTimeSeriesEvent?.closePrice}
-                                displayType={'text'}
-                                thousandSeparator={true}
-                                fixedDecimalScale={true}
-                                decimalScale={2}
-                                prefix={'$'}/>
-            </td>
-            <td className="text-right">
-                <NumberFormat value={latestDailyTimeSeriesEvent?.volume}
-                              displayType={'text'}
-                              thousandSeparator={true}/>
-            </td>
-            <td className="text-center">
-                <StockPriceDirection openPrice={latestDailyTimeSeriesEvent?.openPrice}
-                                     closePrice={latestDailyTimeSeriesEvent?.closePrice}/>
-            </td>
-        </tr>
-    )
+  return (
+    <tr key={id} className="stock-table-row">
+      <td className="stock-table-cell-name">
+        <Link to={userStockDetailUrl}>{name}</Link>
+      </td>
+      <td className="stock-table-cell-symbol">{symbol}</td>
+      <td className="text-right stock-table-cell-open-price">
+        <CurrencyFormat value={latestDailyTimeSeriesEvent?.openPrice}
+                        displayType={'text'}
+                        thousandSeparator={true}
+                        fixedDecimalScale={true}
+                        decimalScale={2}
+                        prefix={'$'}/>
+      </td>
+      <td className="text-right">
+        <CurrencyFormat value={latestDailyTimeSeriesEvent?.highPrice}
+                        displayType={'text'}
+                        thousandSeparator={true}
+                        fixedDecimalScale={true}
+                        decimalScale={2}
+                        prefix={'$'}/>
+      </td>
+      <td className="text-right">
+        <CurrencyFormat value={latestDailyTimeSeriesEvent?.lowPrice}
+                        displayType={'text'}
+                        thousandSeparator={true}
+                        fixedDecimalScale={true}
+                        decimalScale={2}
+                        prefix={'$'}/>
+      </td>
+      <td className="text-right">
+        <CurrencyFormat value={latestDailyTimeSeriesEvent?.closePrice}
+                        displayType={'text'}
+                        thousandSeparator={true}
+                        fixedDecimalScale={true}
+                        decimalScale={2}
+                        prefix={'$'}/>
+      </td>
+      <td className="text-right">
+        <NumberFormat value={latestDailyTimeSeriesEvent?.volume}
+                      displayType={'text'}
+                      thousandSeparator={true}/>
+      </td>
+      <td className="text-center">
+        <StockPriceDirection openPrice={latestDailyTimeSeriesEvent?.openPrice}
+                             closePrice={latestDailyTimeSeriesEvent?.closePrice}/>
+      </td>
+    </tr>
+  )
 }
 
 StocksListingTableRow.propTypes = {
-    stock: PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        symbol: PropTypes.string.isRequired,
-        latestDailyTimeSeriesEvent: PropTypes.shape({
-            openPrice: PropTypes.number.isRequired,
-            closePrice: PropTypes.number.isRequired,
-            highPrice: PropTypes.number.isRequired,
-            lowPrice: PropTypes.number.isRequired,
-            volume: PropTypes.number.isRequired,
-        }).isRequired
-    }).isRequired,
-    userStockId: PropTypes.string.isRequired
+  stock: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    symbol: PropTypes.string.isRequired,
+    latestDailyTimeSeriesEvent: PropTypes.shape({
+      openPrice: PropTypes.number.isRequired,
+      closePrice: PropTypes.number.isRequired,
+      highPrice: PropTypes.number.isRequired,
+      lowPrice: PropTypes.number.isRequired,
+      volume: PropTypes.number.isRequired,
+    }).isRequired
+  }).isRequired,
+  userStockId: PropTypes.string.isRequired
 };
 
 export default StocksListingTableRow;
